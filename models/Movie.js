6f8a50871592a1a9397d1604d8b081b511a477da
Ally M <alexandra.miklos.i@gmail.com>
2019-08-04 18:09:45 +0300

    Add movie Model

---
+++ models/Movie.js
@@ -0,0 +1,27 @@
+var baseUrl = "https://ancient-caverns-16784.herokuapp.com";
+
+window.Movie = function Movie(id = "") {
+  this.id = id;
+};
+
+Movie.prototype.getDetails = function() {
+  var currentMovie = this;
+  if(this.id == ""){
+    throw new Error("Invalid movie id");
+  }
+  return fetch(baseUrl + "/movies/" + this.id).then(async function(response) {
+    if (response.ok) {
+      var data = await response.json();
+      for(var propertyName in data) {
+        if (propertyName == "_id") {
+          continue;
+        }
+
+        currentMovie[propertyName] = data[propertyName];
+      }
+      return currentMovie;
+    }
+
+    throw new Error("API request error", response.status);
+  });
+};
