970e2f6865a29fa699490a2d02686b77387a5700
Georgiana Maghear <georgianamaghear@gmail.com>
2019-08-06 20:33:23 +0300

    Add login basic functionality

---
+++ views/Login.js
@@ -0,0 +1,29 @@
+//get modal
+var modal = document.getElementById('display-modal');
+// close the modal if the user click outside of if
+document.getElementById('display-modal').addEventListener("click", function(event){
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+
+//close the modal if the "cancel" button is clicked
+var cancelBtn = document.querySelector(".cancel-btn");
+cancelBtn.onclick = function(event) {
+    modal.style.display = "none";
+}
+
+
+// get username and password
+document.querySelector('.submit-btn').addEventListener("click", function(event){
+    console.log("event", event);
+    var user = document.getElementById('userName').value;
+    console.log(user);
+    var pass = document.getElementById('passWord').value;
+    console.log(pass);
+})
+
+
+
+
