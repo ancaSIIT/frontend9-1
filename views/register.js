647960eaa9e0babb309f1be199338a69abc8278e
unknown <mihai.sereanu@gmail.com>
2019-08-07 15:46:24 +0300

    Coordinated CSS styles with login form. Had to change register html (form > div) for pop-up implementation to work.

--- views/register.js
+++ views/register.js
@@ -1,19 +1,37 @@
-var register = document.querySelector(".registerbtn")
+// Get modal
+var modal = document.getElementById('display-modal');
+
+// Close modal functionality
+document.getElementById('display-modal').addEventListener("click", function(event){
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+var cancelBtn = document.querySelector(".cancel-btn");
+cancelBtn.onclick = function(event) {
+    modal.style.display = "none";
+}
+
+var register = document.querySelector(".registerBtn");
+
+// Register Implementation
 register.addEventListener("click", function() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  var username = document.getElementById("userName").value;
+  var password = document.getElementById("passWord").value;
   var token = localStorage.getItem("accessToken")
   var user = new Auth();
-
+  var confirm = document.get
   user.registerFromAPI(username, password).then(function(data) {
     console.log(data);
     localStorage.setItem("username", username);
     localStorage.setItem("accessToken", data.accessToken);
-    console.log ("Completed registration");
+    var confirm = document.getElementById("status");
+    confirm.innerHTML = "Completed registration successfuly!"
   }).catch(function(response) {
     if (response.status = 409) {
-      var conflict = document.getElementById("rejection")
+      var conflict = document.getElementById("status")
       conflict.innerHTML = "Username already existing. Please try again.";
     }
-  })
-})
+  });
+});
