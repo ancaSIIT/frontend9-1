57cef939362d47ce79581efa29fe81c50fd0dbd0
natalianaghit <natalianaghit@gmail.com>
2019-08-14 11:49:12 +0300

    add helper

--- views/helpers.js
+++ views/helpers.js
@@ -10,9 +10,7 @@ function showButton() {
   var showDelete = document.querySelectorAll('.delete')
   showDelete.forEach(function(button) {
     button.style.display = "block";
-	
-  var hideLogin =  document.getElementById('display-modal-login')style.display = "none";
-  var hideRegister =  document.getElementById('display-modal-register')style.display = "none";
+
   })
   var addButton = document.getElementById('add-game').style.display = "inline-block";
  
